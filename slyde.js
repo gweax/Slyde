0e3fac59cec1590a6226b9fc7844e405fec674ea
Matthias Reuter <mreuter@inovex.de>
2017-06-01 08:30:57 +0200

    script: modernized js

--- slyde.js
+++ slyde.js
@@ -4,144 +4,152 @@
     Do not include your own javascript. Use user.js for that instead.
  */
 
- (function () {
-var slides = document.querySelectorAll("body > section"),
-    numberOfSlides = slides.length;
+(function() {
+    var slides = document.querySelectorAll("body > section"),
+        numberOfSlides = slides.length;
 
-for (var i = 0; i < numberOfSlides; i++) {
-    var slide = slides[i];
-    slide.id = "slide" + (i + 1);
-    
-    var nav = document.createElement("nav");
-    
-    if (i > 0) {
-        var prev = document.createElement("a");
-        prev.appendChild(document.createTextNode("<"));
-        prev.href = "#slide" + i;
-        nav.appendChild(prev);
-    }
-    
-    var current = document.createElement("span");
-    current.appendChild(document.createTextNode(" " + (i + 1) + " "));
-    nav.appendChild(current);
-    
-    if (i < numberOfSlides - 1) {
-        var next = document.createElement("a");
-        next.appendChild(document.createTextNode(">"));
-        next.href = "#slide" + (i + 2);
-        nav.appendChild(next);
-    }
-    
-    slide.appendChild(nav);
-}
+    for (var i = 0; i < numberOfSlides; i++) {
+        var slide = slides[i];
+        slide.id = "slide" + (i + 1);
 
-function getHiddenIncrementElements () {
-    return [].slice.call(document.querySelectorAll(location.hash + " .increment:not(.show)"));
-}
+        var nav = document.createElement("nav");
 
-function getShownIncrementElements () {
-    return [].slice.call(document.querySelectorAll(location.hash + " .increment.show"));
-}
+        if (i > 0) {
+            var prev = document.createElement("a");
+            prev.appendChild(document.createTextNode("<"));
+            prev.href = "#slide" + i;
+            nav.appendChild(prev);
+        }
 
-function addClass (element, className) {
-    if (element.classList) {
-        element.classList.add(className);
-    }
-    else {
-        element.className = element.className + " " + className;
-    }
-}
+        var current = document.createElement("span");
+        current.appendChild(document.createTextNode(" " + (i + 1) + " "));
+        nav.appendChild(current);
 
-function removeClass (element, className) {
-    if (element.classList) {
-        element.classList.remove(className);
-    }
-    else {
-        element.className = element.className.replace(className, "").trim();
-    }
-}
+        if (i < numberOfSlides - 1) {
+            var next = document.createElement("a");
+            next.appendChild(document.createTextNode(">"));
+            next.href = "#slide" + (i + 2);
+            nav.appendChild(next);
+        }
 
-function forward() {
-    var hidden, nextSlide;
-    
-    hidden = getHiddenIncrementElements();
-    
-    if (hidden.length) {
-        addClass(hidden[0], "show");
+        slide.appendChild(nav);
     }
-    else {
-        nextSlide = +location.hash.substring(6) + 1;
-        if (nextSlide <= numberOfSlides) {
-            location.hash = "#slide" + (nextSlide);
+
+    function getHiddenIncrementElements(onCurrentSlideOnly) {
+        var root = onCurrentSlideOnly ? document.querySelector(location.hash) : document.body;
+
+        return Array.apply(null, root.querySelectorAll(".increment:not(.show)"));
+    }
+
+    function getShownIncrementElements(onCurrentSlideOnly) {
+        var root = onCurrentSlideOnly ? document.querySelector(location.hash) : document.body;
+
+        return Array.apply(null, root.querySelectorAll(".increment.show"));
+    }
+
+    function addClass(element, className) {
+        if (element.classList) {
+            element.classList.add(className);
+        } else {
+            element.className = element.className + " " + className;
         }
     }
-}
 
-function fastForward() {
-    var hidden = getHiddenIncrementElements();
-    
-    if (hidden.length) {
-        hidden.forEach(function (inc) {
-            addClass(inc, "show");
-        });
-    }
-    else if (+location.hash.substring(6) < numberOfSlides) {
-        location.hash = "#slide" + numberOfSlides;
-    }
-}
-
-function rewind() {
-    var shown, previousSlide;
-    
-    shown = getShownIncrementElements();
-    
-    if (shown.length) {
-        removeClass(shown.pop(), "show");
-    }
-    else {
-        previousSlide = +location.hash.substring(6) - 1;
-        if (previousSlide > 0) {
-            location.hash = "#slide" + previousSlide;
+    function removeClass(element, className) {
+        if (element.classList) {
+            element.classList.remove(className);
+        } else {
+            element.className = element.className.replace(className, "").trim();
         }
     }
-}
 
-function fastRewind() {
-    var shown = getShownIncrementElements();
-    
-    if (shown.length) {
-        shown.forEach(function (inc) {
-            removeClass(inc, "show");
-        });
+    function forward() {
+        var hidden, nextSlide;
+
+        hidden = getHiddenIncrementElements(true);
+
+        if (hidden.length) {
+            hidden[0].classList.add("show");
+        } else {
+            nextSlide = +location.hash.substring(6) + 1;
+
+            if (nextSlide <= numberOfSlides) {
+                location.hash = "#slide" + (nextSlide);
+            }
+        }
     }
-    else if (location.hash !== "#slide1") {
+
+    function fastForward() {
+        var hidden = getHiddenIncrementElements();
+
+        if (hidden.length) {
+            hidden.forEach(function(inc) {
+                inc.classList.add("show");
+            });
+        }
+
+        if (+location.hash.substring(6) < numberOfSlides) {
+            location.hash = "#slide" + numberOfSlides;
+        }
+    }
+
+    function rewind() {
+        var shown, previousSlide;
+
+        shown = getShownIncrementElements(true);
+
+        if (shown.length) {
+            shown.pop().classList.remove("show");
+        } else {
+            previousSlide = +location.hash.substring(6) - 1;
+
+            if (previousSlide > 0) {
+                location.hash = "#slide" + previousSlide;
+            }
+        }
+    }
+
+    function fastRewind() {
+        var shown = getShownIncrementElements();
+
+        if (shown.length) {
+            shown.forEach(function(inc) {
+                inc.classList.remove("show");
+            });
+        }
+
+        if (location.hash !== "#slide1") {
+            location.hash = "#slide1";
+        }
+    }
+
+
+    document.addEventListener("keyup", function(event) {
+        if (/input|textarea/i.test(event.target.nodeName) && !(event.ctrlKey && event.shiftKey)) {
+            return;
+        }
+
+        var key = event.keyCode;
+
+        // Home
+        if (key === 36) {
+            fastRewind();
+        }
+        // End
+        else if (key === 35) {
+            fastForward();
+        }
+        // left arrow or page up
+        else if (key === 37 || key === 33) {
+            rewind();
+        }
+        // right arrow or page down
+        else if (key === 39 || key === 34) {
+            forward();
+        }
+    }, false);
+
+    if (location.hash.indexOf("#slide") !== 0) {
         location.hash = "#slide1";
     }
-}
-
-
-document.addEventListener("keyup", function (event) { 
-    var key = event.keyCode;
-        
-    // Home
-    if (key === 36) {
-        fastRewind();
-    }
-    // End
-    else if (key === 35) {
-        fastForward();
-    }
-    // left arrow or page up
-    else if (key === 37 || key === 33) {
-        rewind();
-    }
-    // right arrow or page down
-    else if (key === 39 || key === 34) {
-        forward();
-    }
-}, false);
-
-if (location.hash.indexOf("#slide") !== 0) {
-    location.hash = "#slide1";
-}
 })();
